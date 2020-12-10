7df7e79c22e07ae3d41638d2ed6b23bb52e2a0d3
Christian Hoeoek <61501410+hoeoek@users.noreply.github.com>
2020-12-10 22:49:29 +0100

    found answer to part 1

--- Day8/8.py
+++ Day8/8.py
@@ -8,4 +8,23 @@ def load_data():
     return instructions
 
 
-load_data()
+instructions = load_data()
+
+
+running = True
+current_step = 0
+accumulator = 0
+visited = []
+
+while running:
+    if current_step in visited:
+        print(accumulator)
+        running = False
+    visited.append(current_step)
+    if instructions[current_step][0] == 'acc':
+        accumulator += int(instructions[current_step][1])
+        current_step += 1
+    if instructions[current_step][0] == 'jmp':
+        current_step += int(instructions[current_step][1])
+    if instructions[current_step][0] == 'nop':
+        current_step += 1
