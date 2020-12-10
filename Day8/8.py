0cfcf68d5723106674ded0f6e98ff3bd2a17544a
Christian Hoeoek <61501410+hoeoek@users.noreply.github.com>
2020-12-10 23:34:37 +0100

    starting with part 2. instructions should be modified, fed and checked?

--- Day8/8.py
+++ Day8/8.py
@@ -8,23 +8,51 @@ def load_data():
     return instructions
 
 
+def case_one():
+    instructions = load_data()
+    current_step = 0
+    accumulator = 0
+    visited = []
+    while True:
+        if current_step in visited:
+            return accumulator
+        visited.append(current_step)
+        if instructions[current_step][0] == 'acc':
+            accumulator += int(instructions[current_step][1])
+            current_step += 1
+        if instructions[current_step][0] == 'jmp':
+            current_step += int(instructions[current_step][1])
+        if instructions[current_step][0] == 'nop':
+            current_step += 1
+
+
+def check_if_not_loop(mod_instruction):
+    instructions = mod_instruction
+    current_step = 0
+    accumulator = 0
+    visited = []
+
+    while True:
+        if current_step == len(instructions):
+            print('Program didnt loop!')
+            return accumulator
+        if current_step in visited:
+            print('Program looped at step {}'.format(current_step))
+            return accumulator
+        visited.append(current_step)
+        if instructions[current_step][0] == 'acc':
+            accumulator += int(instructions[current_step][1])
+            current_step += 1
+        if instructions[current_step][0] == 'jmp':
+            current_step += int(instructions[current_step][1])
+        if instructions[current_step][0] == 'nop':
+            current_step += 1
+
 instructions = load_data()
 
+check_if_not_loop(instructions)
 
-running = True
-current_step = 0
-accumulator = 0
-visited = []
 
-while running:
-    if current_step in visited:
-        print(accumulator)
-        running = False
-    visited.append(current_step)
-    if instructions[current_step][0] == 'acc':
-        accumulator += int(instructions[current_step][1])
-        current_step += 1
-    if instructions[current_step][0] == 'jmp':
-        current_step += int(instructions[current_step][1])
-    if instructions[current_step][0] == 'nop':
-        current_step += 1
+def batch_of_instructions():
+    batch = []
+    pass
